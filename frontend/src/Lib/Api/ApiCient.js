ca32d82d5d9d43c66a116901ac9c1fde968fa3b8
cumar-dev <your_email@example.com>
2026-07-20 11:28:19 +0300

    making local url and server url

--- frontend/src/Lib/Api/ApiCient.js
+++ frontend/src/Lib/Api/ApiCient.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { useAuthStore } from "../Store/AuthStore";
 export const Api = axios.create({
-    baseURL: import.meta.env.VITE_API_URL_DEVELOPMENT,
+    baseURL: import.meta.env.VITE_API_URL,
     headers: {
         "Content-Type": "application/json"
     },
