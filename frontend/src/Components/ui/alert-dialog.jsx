50be06e2cfe507ca952b3d279a1abc585fd52fc3
cumar-dev <your_email@example.com>
2026-07-20 09:30:00 +0300

    fix ui issue from shadCn

--- frontend/src/Components/ui/alert-dialog.jsx
+++ frontend/src/Components/ui/alert-dialog.jsx
@@ -2,7 +2,7 @@ import * as React from "react"
 import { AlertDialog as AlertDialogPrimitive } from "@base-ui/react/alert-dialog"
 
 import { cn } from "@/Lib/utils";
-import { Button } from "@/components/ui/button"
+import { Button } from "@/Components/ui/button"
 
 function AlertDialog({
   ...props
